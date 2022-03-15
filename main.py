cd33be0605f029d3e3d78e362e28e4db150c1118
matildeopbravo <matildeopbravo@gmail.com>
2022-03-15 20:02:37 +0000

    Added functions to parse header and create objects of field class

--- main.py
+++ main.py
@@ -1,20 +1,88 @@
-import re, sys
-from dataclasses import dataclass
+import sys, re
+
+class Field:
+    def __init__(self, name, minimum = 1, maximum = 1, agg_fun = lambda x:x):
+        self.name = name
+        self.min =  minimum
+        self.max = maximum
+        self.agg_fun = agg_fun
+
+    def __str__(self):
+         return "name: %s , min: %s, max: %s, agg_fun: %s" % (self.name,
+                 self.min, self.max , self.agg_fun.__name__)
 
 
-data = []
+fields = []
+#new_field = Field("names", 0, 10, lambda l : ' '.join(l))
+str_regex = r'[a-zA-Z_]+\w*'
+rng_regex = "(\w+){(\d+)(?:,(\d+))?}"
 
-def parse_header(header):
-    for field in header:
-        rng = r"\{\d+(,\d+)?\}"
-        if re.search(rng, field):
-            if re.search(rng+"::\w+"):
-                # range with associate function
+token_specification = [
+        ('AGG_FUNCTION', r'(%s|%s)(::(\w+))' % (str_regex, rng_regex)),
+        ('RANGE', rng_regex),
+        ('STR', str_regex),
+]
 
-        else:
-            # normal  ou invalido se tiver tipo um {
+tok_regex = '|'.join(('(?P<%s>%s)') % pair for pair in token_specification)
 
 
-keys = sys.stdin.readline().split(',')
-for line in sys.stdin:
-    print(line)
+#def create_field(unparsed_field):
+#    r = re.finditer(tok_regex, sys.stdin.readline())
+#    for i in r:
+#        dic = i.groupdict()
+#        print(dic)
+#    # create a Field   and return it
+#    #return field
+#
+## reaad header
+#m = sys.stdin.readline()
+#print(m)
+#header = [ create_field(field) for field in m ]
+
+
+def media(l):
+    return sum(l) /len(l)
+
+
+# recebe nome{...}
+def parse_range(line):
+    r = re.search(rng_regex, line)
+    maybe_max = r.group(3)
+    minimum = r.group(2)
+    maximum = maybe_max if maybe_max else minimum
+    return {"name": r.group(1), "min": minimum, "max": maximum}
+
+
+def create_field(dic):
+    field = None
+    fun = None
+    minimum = maximum = 1
+    if dic['STR']:
+        field = Field(dic['STR'])
+    else:
+        if dic['AGG_FUNCTION']:
+            r = dic['AGG_FUNCTION'].split('::')
+            fun = eval(r[1])
+            # se tiver um range {x,y} ou {x}
+            dict_range = parse_range(r[0])
+        elif dic['RANGE']:
+            dict_range = parse_range(dic['RANGE'])
+
+        if (dict_range):
+            minimum = dict_range['min']
+            maximum = dict_range['max']
+            field = Field(dict_range['name'], minimum, maximum)
+
+        if(fun):
+            field.agg_fun = fun
+
+    print(field)
+    fields.append(field)
+
+def parse_header():
+    r = re.finditer(tok_regex, sys.stdin.readline())
+    for i in r:
+            dic = i.groupdict()
+            create_field(dic)
+
+parse_header()
